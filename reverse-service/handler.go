8bf0238569d05ae76162bff76f8b9e8dadd2269b
Ruilin Geng <1585363499@qq.com>
2023-07-14 22:44:46 +0800

    reverse实现

--- reverse-service/handler.go
+++ reverse-service/handler.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	reverse "api-gateway/reverse-service/kitex_gen/reverse"
+	"api-gateway/reverse-service/kitex_gen/reverse"
 	"context"
 )
 
@@ -10,6 +10,18 @@ type ReverseServiceImpl struct{}
 
 // ReverseString implements the ReverseServiceImpl interface.
 func (s *ReverseServiceImpl) ReverseString(ctx context.Context, req *reverse.ReverseRequest) (resp *reverse.ReverseResponse, err error) {
-	// TODO: Your code here...
+	resp = &reverse.ReverseResponse{
+		ReversedString: Reverse(req.InputString),
+	}
 	return
+}
+
+func Reverse(s string) string {
+	a := []rune(s)
+	// todo: 使用微服务
+	lenA := len(a)
+	for i, j := 0, lenA-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+	return string(a)
 }
